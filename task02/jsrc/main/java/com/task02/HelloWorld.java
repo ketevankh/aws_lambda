9f4b768d0688cfd64e67978e12e7bbe1709f334a
ketevankh <kkhac20@freeuni.edu.ge>
2024-09-03 13:47:00 +0400

    done

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,51 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(
+    lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = false,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+// public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+//
+// 	public Map<String, Object> handleRequest(Object request, Context context) {
+// 		System.out.println("Hello from lambda");
+// 		Map<String, Object> resultMap = new HashMap<String, Object>();
+// 		resultMap.put("statusCode", 200);
+// 		resultMap.put("body", "Hello from Lambda");
+// 		return resultMap;
+// 	}
+// }
+public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
+		Map<String, Object> resultMap = new HashMap<>();
+		String path = (String) request.get("path");
+		String method = (String) request.get("httpMethod");
+
+		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+			resultMap.put("statusCode", 200);
+			resultMap.put("message", "Hello from Lambda");
+		} else {
+			resultMap.put("statusCode", 400);
+			resultMap.put("message", String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method));
+		}
+
+		return resultMap;
+	}
+}
